70a1baa35b956589ffd9a7f080bc7985cb2ec560
Deepak-Kumar-784 <deepakkumardipu659@gmail.com>
2026-02-03 01:15:37 +0530

    ci: update workflows and requirements for HTML/CSS checks and script separation

---
+++ templates/script.js
@@ -0,0 +1,9 @@
+function goToDocs() {
+  window.location.href = "/docs";
+}
+function goToSource() {
+  window.open("https://github.com/DeepakKumarBehera/your-repo", "_blank");
+}
+function goToApi() {
+  window.location.href = "/generate?text=Hello%20World";
+}
